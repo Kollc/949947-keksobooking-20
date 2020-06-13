09e5c5d4f1e519bac97f6fbe7e3f4c0a1079fb21
Kollc <46793445+Kollc@users.noreply.github.com>
2020-06-13 23:15:28 +0300

    Реализовал скрипт показа характеристика объявления

--- js/main.js
+++ js/main.js
@@ -26,11 +26,18 @@ var OPTIONS = {
     'Много кокосовых пальм вокруг',
     'Красивые соседки'
   ],
-  TYPES: [
-    'palace',
-    'flat',
-    'house',
-    'bungalo'
+  TYPES: [{
+    palace: 'Дворец'
+  },
+  {
+    flat: 'Квартира'
+  },
+  {
+    house: 'Дом'
+  },
+  {
+    bungalo: 'Бунгало'
+  }
   ],
   CHECKINS: [
     '12:00',
@@ -79,6 +86,77 @@ var OPTIONS = {
   }
 };
 
+// Функция проверяет соответстиве элемента в объекте массива и возвращает русифицированную версию (пришлось написать вот это, тк ассоциативные массивы только с ES6)
+var russificationTypes = function (obj) {
+  var types = ['palace', 'flat', 'house', 'bungalo'];
+  var currentType;
+  for (var i = 0; i < types.length; i++) {
+    if (obj.hasOwnProperty(types[i])) {
+      currentType = obj[types[i]];
+    }
+  }
+
+  return currentType;
+};
+
+// Функция заполняет элементы списка текстом или давбляет в src путь к картинкам и удаляет пустые
+var createListElement = function (mass, parentElement, SelectorElements, image) {
+  if (!image) {
+    mass.forEach(function (item) {
+      var childElement = parentElement.querySelector(SelectorElements + '--' + item);
+      childElement.textContent = item;
+    });
+
+    var childAllElements = parentElement.querySelectorAll(SelectorElements);
+    childAllElements.forEach(function (item) {
+      if (item.textContent === '') {
+        item.remove();
+      }
+    });
+  } else {
+    mass.forEach(function (item, index) {
+      var childElement = parentElement.querySelector(SelectorElements);
+      if (index >= 1) {
+        childElement = childElement.cloneNode(true);
+      }
+
+      childElement.src = item;
+      parentElement.appendChild(childElement);
+    });
+  }
+
+  return parentElement;
+};
+
+// Функция возвращает элемент только с уникальными значениями
+var uniqueElementsMass = function (arr) {
+
+  for (var l = 0; l < arr.length; l++) {
+
+    for (var z = l + 1; z < arr.length; z++) {
+      if (arr[l] === arr[z]) {
+        delete arr[z];
+      }
+    }
+  }
+
+  return arr.filter(function (e) {
+    return e !== null;
+  });
+};
+
+// Функция возвращает массив  с радомными элементами
+var randomGenerateSomeElement = function (mass) {
+  var j = randomGenerate(0, mass.length); // перменная кол-ва элементов
+  var newMass = [];
+
+  for (var i = 0; i <= j; i++) {
+    newMass.push(mass[randomGenerate(0, mass.length)]);
+  }
+
+  return uniqueElementsMass(newMass);
+};
+
 // функция возращает рандомное число в определенном интервале
 var randomGenerate = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -94,14 +172,14 @@ var createAdsObj = function (i) {
       'title': OPTIONS.TITLES[randomGenerate(0, OPTIONS.TITLES.length)],
       'address': randomGenerate(OPTIONS.LOCATION.X.MIN, OPTIONS.LOCATION.X.MAX) + ',' + randomGenerate(OPTIONS.LOCATION.Y.MIN, OPTIONS.LOCATION.Y.MAX),
       'price': randomGenerate(OPTIONS.PRICE.MIN, OPTIONS.PRICE.MAX),
-      'type': OPTIONS.FEATURES[randomGenerate(0, OPTIONS.FEATURES.length)],
+      'type': OPTIONS.TYPES[randomGenerate(0, OPTIONS.TYPES.length)],
       'rooms': randomGenerate(OPTIONS.ROOMS.MIN, OPTIONS.ROOMS.MAX),
       'guests': randomGenerate(OPTIONS.GUESTS.MIN, OPTIONS.GUESTS.MAX),
       'checkin': OPTIONS.CHECKINS[randomGenerate(0, OPTIONS.CHECKINS.length)],
       'checkout': OPTIONS.CHECKOUTS[randomGenerate(0, OPTIONS.CHECKOUTS.length)],
-      'features': OPTIONS.FEATURES[randomGenerate(0, OPTIONS.FEATURES.length)],
+      'features': randomGenerateSomeElement(OPTIONS.FEATURES),
       'description': OPTIONS.DESCRIPTION[randomGenerate(0, OPTIONS.DESCRIPTION.length)],
-      'photos': OPTIONS.PHOTOS[randomGenerate(0, OPTIONS.PHOTOS.length)],
+      'photos': randomGenerateSomeElement(OPTIONS.PHOTOS)
     },
     'location': {
       'x': randomGenerate(OPTIONS.LOCATION.X.MIN, OPTIONS.LOCATION.X.MAX),
@@ -115,22 +193,41 @@ var createAdsObj = function (i) {
 // Функция добавляет метки объявлений в DOM
 var addAds = function () {
 
-  var fragment = document.createDocumentFragment();
+  var fragmentLabel = document.createDocumentFragment();
+  var fragmentCard = document.createDocumentFragment();
+
   var mapPins = document.querySelector('.map__pins'); // блок с картой, куда нужно добавить объявления
-  document.querySelector('.map').classList.remove('map--faded'); // делаем карту активной
-  var templateLabel = document.querySelector('#pin').content.querySelector('.map__pin'); // получаем шаблон из верстки
+  var mapContainer = document.querySelector('.map__filters-container');
+  var map = document.querySelector('.map');
+  map.classList.remove('map--faded'); // делаем карту активной
+
+  var templateLabel = document.querySelector('#pin').content.querySelector('.map__pin'); // получаем шаблон метки из верстки
+  var templateCard = document.querySelector('#card').content.querySelector('.map__card'); // получаем шаблон каточки объяевления из верстки
 
   for (var i = 0; i < TOTAL_ADS; i++) {
-    var elem = templateLabel.cloneNode(true);
-    elem.querySelector('img').src = createAdsObj(i).author.avatar;
-    elem.querySelector('img').alt = createAdsObj(i).offer.title;
-    elem.style.left = (createAdsObj(i).location.x - WIDTH_ADS / 2) + 'px'; // устанавливаем координаты метки с учетом ее размеров
-    elem.style.top = (createAdsObj(i).location.y - HEIGHT_ADS) + 'px'; // устанавливаем координаты метки с учетом ее размеров
+    var elemLabel = templateLabel.cloneNode(true);
+    elemLabel.querySelector('img').src = createAdsObj(i).author.avatar;
+    elemLabel.querySelector('img').alt = createAdsObj(i).offer.title;
+    elemLabel.style.left = (createAdsObj(i).location.x - WIDTH_ADS / 2) + 'px'; // устанавливаем координаты метки с учетом ее размеров
+    elemLabel.style.top = (createAdsObj(i).location.y - HEIGHT_ADS) + 'px'; // устанавливаем координаты метки с учетом ее размеров
+    fragmentLabel.appendChild(elemLabel);
 
-    fragment.appendChild(elem);
+    var elemCard = templateCard.cloneNode(true);
+    elemCard.querySelector('.popup__avatar').src = createAdsObj(i).author.avatar;
+    elemCard.querySelector('.popup__title').textContent = createAdsObj(i).offer.title;
+    elemCard.querySelector('.popup__text--address').textContent = createAdsObj(i).offer.address;
+    elemCard.querySelector('.popup__text--price').textContent = createAdsObj(i).offer.price + '₽/ночь';
+    elemCard.querySelector('.popup__type').textContent = russificationTypes(createAdsObj(i).offer.type);
+    elemCard.querySelector('.popup__text--capacity').textContent = createAdsObj(i).offer.rooms + ' комнаты для ' + createAdsObj(i).offer.guests + ' гостей';
+    elemCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + createAdsObj(i).offer.checkin + ', выезд до ' + createAdsObj(i).offer.checkout;
+    elemCard.appendChild(createListElement(createAdsObj(i).offer.features, elemCard.querySelector('.popup__features'), '.popup__feature', false));
+    elemCard.querySelector('.popup__description').textContent = createAdsObj(i).offer.description;
+    elemCard.appendChild(createListElement(createAdsObj(i).offer.photos, elemCard.querySelector('.popup__photos'), '.popup__photo', true));
+    fragmentCard.appendChild(elemCard);
   }
-  mapPins.appendChild(fragment);
-};
 
+  mapPins.appendChild(fragmentLabel);// добавляем метки объявлений на карту
+  map.insertBefore(fragmentCard, mapContainer); // добавляем объявления на страницу
+};
 
 addAds();
